80028b2c12be06daefc7622a5b5438cb6a5b92ef
Suraj Santosh Pawar <surajpawar913@gmail.com>
2023-05-23 13:09:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,68 +71,6 @@ run("Full stack web developer");
 
 
 
-<!DOCDOCTYPETYPE html>
-<html>
-<head>
-  <title>Tag Input Field</title>
-
-    .tag {
-      display: inline-block;
-      background-color: #f2f2f2;
-      padding: 5px;
-      margin: 5px;
-      border-radius: 5px;
-    }
-  </style>
-</head>
-<body>
-  <input type="text" id="tagInput" placeholder="Enter tags" />
-  <div id="tagContainer"></div>
-
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      var tags = []; // Array to store the tags
-
-      // Function to add a tag
-      function addTag(tag) {
-        var tagElement = $('<div class="tag">' + tag + '</div>');
-        $('#tagContainer').append(tagElement);
-      }
-
-      // Function to handle tag input
-      function handleTagInput() {
-        var tag = $('#tagInput').val().trim();
-
-        if (tag !== '') {
-          tags.push(tag); // Add tag to the array
-          addTag(tag); // Add tag to the container
-          $('#tagInput').val(''); // Clear the input field
-        }
-      }
-
-      // Handle tag input when Enter key is pressed
-      $('#tagInput').on('keydown', function(event) {
-        if (event.which === 13 || event.keyCode === 13) {
-          handleTagInput();
-          return false;
-        }
-      });
-
-      // Handle tag input when a comma is entered
-      $('#tagInput').on('input', function() {
-        var value = $(this).val();
-        if (value.slice(-1) === ',') {
-          handleTagInput();
-        }
-      });
-    });
-  </script>
-</body>
-</html>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -141,25 +79,38 @@ run("Full stack web developer");
 </head>
 <body>
   <input type="text" id="tagInput" placeholder="Enter tags">
-  <button id="addTagButton">Add Tag</button>
   <ul id="tagList"></ul>
 
   <script>
     $(document).ready(function() {
       var tags = [];
 
-      $('#addTagButton').click(function() {
-        var tag = $('#tagInput').val().trim();
+      $('#tagInput').on('keydown', function(event) {
+        if (event.keyCode === 13) { // Enter key
+          event.preventDefault();
+          var tag = $(this).val().trim();
 
-        if (tag !== '') {
-          tags.push(tag);
-          $('#tagList').append('<li>' + tag + '</li>');
-          $('#tagInput').val('');
+          if (tag !== '') {
+            tags.push(tag);
+            $(this).val('');
+            updateTagList();
+          }
         }
       });
+
+      function updateTagList() {
+        $('#tagList').empty();
+        for (var i = 0; i < tags.length; i++) {
+          $('#tagList').append('<li>' + tags[i] + '</li>');
+        }
+      }
     });
   </script>
 </body>
 </html>
+
+
+
+
 
 
